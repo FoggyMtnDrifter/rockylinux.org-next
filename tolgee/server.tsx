dca1623bd56ce2c612a12dc10ecbc60eefee0034
Michael Kinder <me@mwk.email>
2024-01-22 17:50:56 -0500

    first commit

---
+++ tolgee/server.tsx
@@ -0,0 +1,39 @@
+import { cache } from "react";
+import { useLocale } from "next-intl";
+
+import { TolgeeBase, ALL_LOCALES, getStaticData } from "./shared";
+
+// wrapping in `cache` function will ensure
+// that we are sharing the instance within a single request
+export const getTolgeeInstance = cache(async (locale: string) => {
+  const tolgee = TolgeeBase().init({
+    // include all static data on the server, as the bundle size is not a concern here
+    staticData: await getStaticData(ALL_LOCALES),
+    observerOptions: {
+      // include full information about the key into the watermark
+      // make sure you have newest SDK for this feature
+      fullKeyEncode: true,
+    },
+    // Locale is already detected by next-intl package
+    language: locale,
+    // providing custom fetch function, which will disable default caching
+    fetch: async (input, init) => {
+      return fetch(input, { ...init, next: { revalidate: 0 } });
+    },
+  });
+
+  await tolgee.run();
+
+  return tolgee;
+});
+
+export const getTolgee = async () => {
+  const locale = useLocale();
+  const tolgee = await getTolgeeInstance(locale);
+  return tolgee;
+};
+
+export const getTranslate = async () => {
+  const tolgee = await getTolgee();
+  return tolgee.t;
+};
