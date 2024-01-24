e71f1fcc0fe0eace92f98efd97e87a35e344a301
Michael Kinder <me@mwk.email>
2024-01-24 07:31:46 -0500

    remove reference to non-existent component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,5 @@
 import FeatureOne from "@/components/home/feature-one";
-import FeatureTwo from "@/components/home/feature-two";
 import Hero from "@/components/home/hero";
-import Image from "next/image";
 
 export default async function Home() {
   return (
