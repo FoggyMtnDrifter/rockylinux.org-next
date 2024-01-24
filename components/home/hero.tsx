7ac3e788c377f279906cbc2d29eed27185308dda
Michael Kinder <me@mwk.email>
2024-01-24 05:20:22 -0500

    fix padding on hero

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -4,7 +4,7 @@ import { Button } from "../ui/button";
 export default function Hero() {
   return (
     <div className="relative isolate pt-14">
-      <div className="py-24 sm:py-32 lg:pb-40">
+      <div className="sm:py-12 lg:pb-40">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl text-center">
             <h1 className="text-4xl font-bold tracking-tight sm:text-6xl font-display">
